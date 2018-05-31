2423ab66f9a2cce55fddc4ef594231499154158e
sayands <sayandsarkar.1997@gmail.com>
2018-05-31 15:45:22 +0300

    Added More Layers and Tuned Hyperparameters for higher accuracy

--- ImageClassification/CNN.py
+++ ImageClassification/CNN.py
@@ -4,23 +4,32 @@
 
 # Importing the Keras libraries and packages
 from keras.models import Sequential
-from keras.layers import Conv2D,MaxPooling2D,Flatten,Dense
+from keras.layers import Conv2D,MaxPooling2D,Flatten,Dense, Dropout
 from keras.preprocessing.image import ImageDataGenerator
 
 # Initialising the CNN
 classifier = Sequential()
 
 # Step 1 - Convolution
-classifier.add(Conv2D(filters = 32, kernel_size = (3,3), input_shape = (64, 64, 3), activation = 'relu'))
+classifier.add(Conv2D(filters = 32, kernel_size = (3,3), input_shape = (128, 128, 3), activation = 'relu'))
 
 # Setp 2 - Pooling
 classifier.add(MaxPooling2D(pool_size = (2, 2)))
+
+# 2nd - Conv + Pooling
+classifier.add(Conv2D(32, (3, 3), activation='relu'))
+classifier.add(MaxPooling2D(pool_size=(2, 2)))
+
+# 3rd - Conv + Pooling
+classifier.add(Conv2D(32, (3, 3), activation='relu'))
+classifier.add(MaxPooling2D(pool_size=(2, 2)))
 
 # Step 3 - Flattening
 classifier.add(Flatten())
 
 # Step 4 - Fully Connected Layer
 classifier.add(Dense(units = 128, activation = 'relu'))
+classifier.add(Dropout(rate = 0.5))
 classifier.add(Dense(units = 1, activation = 'sigmoid'))
 
 # Compiling the CNN
@@ -37,20 +46,20 @@ train_datagen = ImageDataGenerator(
 test_datagen = ImageDataGenerator(rescale=1./255)
 
 training_set = train_datagen.flow_from_directory(
-        'dataset/training_set',
-        target_size=(64, 64),
+        '/my_data/dataset/training_set',
+        target_size=(128, 128),
         batch_size=32,
         class_mode='binary')
 
 test_set = test_datagen.flow_from_directory(
-        'dataset/test_set',
-        target_size=(64, 64),
+        '/my_data/dataset/test_set',
+        target_size=(128, 128),
         batch_size=32,
         class_mode='binary')
 
 classifier.fit_generator(
         training_set,
-        steps_per_epoch=8000,
+        steps_per_epoch=8000/32,
         epochs=25,
         validation_data = test_set,
-        validation_steps=2000)
+        validation_steps=2000/32)
