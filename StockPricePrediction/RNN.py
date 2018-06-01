48a9c7d9b3b5e1aa0a4777318d9739ab2f1fca86
sayands <sayandsarkar.1997@gmail.com>
2018-06-01 17:07:43 +0300

    Added more predictors and tuned the model for higher accuracy

--- StockPricePrediction/RNN.py
+++ StockPricePrediction/RNN.py
@@ -9,24 +9,36 @@ import matplotlib.pyplot as plt
 
 # Importing the dataset
 dataset_train = pd.read_csv('Google_Stock_Price_Train.csv')
-training_set = dataset_train.iloc[:, 1:2].values
+training_set = dataset_train.iloc[:, 1:5].values
+
+#Preprocess data for training by removing all commas
+cols = list(dataset_train)[1:5]
+dataset_train = dataset_train[cols].astype(str)
+for i in cols:
+    for j in range(0,len(dataset_train)):
+        dataset_train[i][j] = dataset_train[i][j].replace(",","")
+ 
+dataset_train = dataset_train.astype(float)
+ 
+ 
+training_set = dataset_train.as_matrix() # Using multiple predictors.
 
 # Feature Scaling
 from sklearn.preprocessing import MinMaxScaler
 sc = MinMaxScaler(feature_range = (0, 1))
 training_set_scaled = sc.fit_transform(training_set)
 
-# Creating the data structure with 80 timestamps and 1 output
+# Creating the data structure with 60 timestamps and 1 output
 X_train = []
 y_train = []
-for i in range(120, 1258):
-    X_train.append(training_set_scaled[i-120:i, 0])
+for i in range(60, 1258):
+    X_train.append(training_set_scaled[i-60:i, 0:5])
     y_train.append(training_set_scaled[i, 0])
 
 X_train, y_train = np.array(X_train), np.array(y_train)
 
 # Reshaping
-X_train = np.reshape(X_train, (X_train.shape[0], X_train.shape[1], 1))
+#X_train = np.reshape(X_train, (X_train.shape[0], X_train.shape[1], 4))
 
 # Part II - Building the RNN
 
@@ -38,26 +50,29 @@ from keras.layers import Dense, LSTM, Dropout
 regressor = Sequential()
 
 # Adding the first LSTM layer and Dropout Regularisation
-regressor.add(LSTM(units = 50, return_sequences = True, input_shape = (X_train.shape[1], 1)))
+regressor.add(LSTM(units = 75, return_sequences = True, input_shape = (X_train.shape[1], 4)))
 regressor.add(Dropout(rate = 0.2))
 
 # Adding the second LSTM layer and Dropout Regularisation
-regressor.add(LSTM(units = 50, return_sequences = True))
+regressor.add(LSTM(units = 75, return_sequences = True))
 regressor.add(Dropout(rate = 0.2))
 
 # Adding the third LSTM lasyer and Dropout Regularisation
-regressor.add(LSTM(units = 50, return_sequences = True))
+regressor.add(LSTM(units = 75, return_sequences = True))
 regressor.add(Dropout(rate = 0.2))
 
 # Adding the fourth LSTM layer and Dropout Regularisation
-regressor.add(LSTM(units = 50, return_sequences = False))
+regressor.add(LSTM(units = 75, return_sequences = False))
 regressor.add(Dropout(rate = 0.2))
 
 # Adding the output layer
 regressor.add(Dense(units = 1, activation = 'sigmoid'))
 
 # Compiling the RNN
-regressor.compile(optimizer = 'adam' , loss = 'mean_squared_error')
+import keras
+from keras import optimizers
+optimizer = optimizers.RMSprop(lr=0.001, rho=0.9, epsilon=None, decay=0.0)
+regressor.compile(optimizer = optimizer , loss = 'mean_squared_error')
 
 # Fitting the RNN to the Training Set
 regressor.fit(X_train, y_train, epochs = 100, batch_size = 32)
@@ -69,19 +84,21 @@ dataset_test = pd.read_csv('Google_Stock_Price_Test.csv')
 real_stock_price = dataset_test.iloc[:, 1:2].values
 
 # Getting the predicted stock price of Jan 2017
-dataset_total = pd.concat((dataset_train['Open'], dataset_test['Open']), axis = 0)
+ds_test = dataset_test.iloc[:, 1:5]
+dataset_total = pd.concat((dataset_train, ds_test), axis = 0)
 inputs = dataset_total[len(dataset_total) - len(dataset_test) - 60 :].values
-inputs = inputs.reshape(-1, 1)
+#inputs = inputs.reshape(-1, 1)
 inputs = sc.transform(inputs)
 
 X_test = []
-for i in range(1200, 140):
-    X_test.append(inputs[i-120:i, 0])
-
+for i in range(60, 80):
+    X_test.append(inputs[i-60:i, 0:5])
 X_test = np.array(X_test)
-X_test = np.reshape(X_test, (X_test.shape[0], X_test.shape[1], 1))
+#X_test = np.reshape(X_test, (X_test.shape[0], X_test.shape[1], 4))
 predicted_stock_price = regressor.predict(X_test)
-predicted_stock_price = sc.inverse_transform(predicted_stock_price)
+sc_predict = MinMaxScaler(feature_range=(0,1))
+sc_predict.fit_transform(training_set[:,0:1])
+predicted_stock_price = sc_predict.inverse_transform(predicted_stock_price)
 
 # Visualising the Results
 plt.plot(real_stock_price, color = 'red', label = 'Real Google Stock Price')
