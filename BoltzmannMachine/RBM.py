fc1a1228b080fdc30abaa297df5fdcabf1f884c5
sayands <sayandsarkar.1997@gmail.com>
2018-06-26 15:46:27 +0300

    Initial commit for project

--- BoltzmannMachine/RBM.py
+++ BoltzmannMachine/RBM.py
@@ -1,9 +1,28 @@
-#!/usr/bin/env python2
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Jun 26 13:24:41 2018
+# Boltzmann Machine
 
-@author: sayan
-"""
+# Importing the libraries
+import numpy as np
+import pandas as pd
+import torch
+import torch.nn as nn
+import torch.nn.parallel
+import torch.optim as optim
+import torch.utils.data
+from torch.autograd import Variable
 
-print("Sayan")
+# Importing the dataset
+movies = pd.read_csv("ml-1m/movies.dat", sep='::', header = None, engine = 'python', encoding = 'latin-1')
+users = pd.read_csv("ml-1m/users.dat", sep='::', header = None, engine = 'python', encoding = 'latin-1')
+ratings = pd.read_csv("ml-1m/ratings.dat", sep='::', header = None, engine = 'python', encoding = 'latin-1')
+
+# Preparing the training and test sets
+training_set = pd.read_csv('ml-100k/u1.base', delimiter = '\t')
+training_set = np.array(training_set, dtype= 'int')
+test_set = pd.read_csv('ml-100k/u1.test', delimiter = '\t')
+test_set = np.array(test_set, dtype= 'int')
+
+# Getting the number of users and movies
+nb_users = int(max(max(training_set[:, 0]), max(test_set[:,0])))
+nb_movies = int(max(max(training_set[:, 1]), max(test_set[:,1])))
+
+# Converting the data into an array with users in lines and movies in columns
