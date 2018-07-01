d4ba58f014f73aced50927758ee2245ed4f7954e
sayands <sayandsarkar.1997@gmail.com>
2018-07-01 08:01:22 +0300

    Added initial RBM model

--- BoltzmannMachine/RBM.py
+++ BoltzmannMachine/RBM.py
@@ -26,3 +26,76 @@ nb_users = int(max(max(training_set[:, 0]), max(test_set[:,0])))
 nb_movies = int(max(max(training_set[:, 1]), max(test_set[:,1])))
 
 # Converting the data into an array with users in lines and movies in columns
+def convert(data):
+    new_data = []
+    for id_users in range(1, nb_users+1):
+        id_movies = data[:,1][data[:,0] == id_users]
+        id_ratings = data[:,2][data[:,0] == id_users]
+        ratings = np.zeros(nb_movies)
+        ratings[id_movies - 1] = id_ratings
+        new_data.append(list(ratings))
+    return new_data
+
+training_set = convert(training_set)
+test_set = convert(test_set)
+        
+# Converting the data to torch tensors
+training_set = torch.FloatTensor(training_set)
+test_set = torch.FloatTensor(test_set)
+
+# Converting the ratings into binary ratings 1(Liked) or 0(Not Linked)
+training_set[training_set == 0] = -1
+training_set[training_set == 1] = 0
+training_set[training_set == 2] = 0
+training_set[training_set >=3 ] = 1
+
+test_set[test_set == 0] = -1
+test_set[test_set == 1] = 0
+test_set[test_set == 2] = 0
+test_set[test_set >=3 ] = 1
+
+# Creating the architecture of the Neural Network
+class RBM():
+    def __init__(self, nv, nh):
+        self.W = torch.randn(nh, nv)
+        self.a = torch.randn(1, nh)
+        self.b = torch.randn(1, nv)
+    
+    def sample_h(self, x):
+        wx = torch.mm(x, self.W.t())
+        activation = wx + self.a.expand_as(wx)
+        p_h_given_v = torch.sigmoid(activation)
+        return p_h_given_v, torch.bernoulli(p_h_given_v)
+    
+    def sample_v(self, y):
+        wy = torch.mm(y, self.W)
+        activation = wy + self.b.expand_as(wy)
+        p_v_given_h = torch.sigmoid(activation)
+        return p_v_given_h, torch.bernoulli(p_v_given_h)
+
+    
+  
+
+
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+        
+
+        
+        
+
+
+
+
+
+
+
+
