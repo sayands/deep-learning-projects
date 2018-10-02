ab69a3a095a3c0457a8de4c7b2a1da931b308916
sayands <sayandsarkar.1997@gmail.com>
2018-10-02 15:30:23 +0530

    Defined the CNN architecture for classification

--- Fashion-MNIST/fashion_mnist_recogniser.py
+++ Fashion-MNIST/fashion_mnist_recogniser.py
@@ -35,9 +35,46 @@ plt.show()
 # Reshaping the data
 im_rows = 28
 im_cols = 28
-batch_size = 512
 im_shape = (im_rows, im_cols, 1)
 
 X_train = X_train.reshape(X_train.shape[0], *im_shape)
 X_test = X_test.reshape(X_test.shape[0], *im_shape)
 X_val  = X_val.reshape(X_val.shape[0], *im_shape)
+
+
+# Importing the keras packages
+from keras.models import Sequential
+from keras.layers import Conv2D, MaxPooling2D, Dense, Flatten, Dropout
+from keras.optimizers import Adam
+
+# Defining the Model
+model = Sequential()
+
+model.add(Conv2D(32, kernel_size = 3, activation = 'relu', input_shape = im_shape, kernel_initializer = 'he_normal', name = 'Conv2D-1'))
+model.add(MaxPooling2D(pool_size = 2, name = 'Maxpool'))
+model.add(Dropout(0.25, name = 'Dropout-1'))
+
+model.add(Conv2D(64, kernel_size = 3, activation = 'relu', name = 'Conv2D-2'))
+model.add(Dropout(0.25, name = 'Dropout-2'))
+
+model.add(Conv2D(128, kernel_size = 3, activation = 'relu', name = 'Conv2D-3'))
+model.add(Dropout(0.4, name = 'Dropout-3'))
+
+model.add(Flatten(name = 'Flatten'))
+model.add(Dense(128, activation = 'relu', name = 'Dense'))
+model.add(Dropout(0.4, name = 'Dropout'))
+model.add(Dense(10, activation = 'softmax', name = 'Output'))
+
+
+print(model.summary())
+
+# Hyperparameters
+batch_size = 512
+epochs = 50
+
+# Compiling The Data
+model.compile(loss = 'sparse_categorical_crossentropy', optimizer = 'adam', metrics = ['accuracy'])
+history = model.fit(X_train, y_train, batch_size = batch_size, epochs = epochs, verbose = 1, validation_data = (X_val, y_val))
+
+
+
