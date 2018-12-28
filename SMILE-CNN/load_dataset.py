b50895665a255fef5d7e458e81dbb0b43bbdeb3e
sayands <sayandsarkar.1997@gmail.com>
2018-12-28 13:23:24 +0530

    Fixed Minor Issues and Prepared Dataset for Training

--- SMILE-CNN/load_dataset.py
+++ SMILE-CNN/load_dataset.py
@@ -4,6 +4,8 @@ import time
 from skimage.measure import block_reduce
 from skimage.io import imread 
 from utils.list_all_files import list_all_files 
+from utils.make_mosaic import make_mosaic
+from utils.show_array import show_array
 
 negative_paths = list(list_all_files('./dataset/SMILEsmileD-master/SMILEs/negatives/negatives7', ['.jpg']))
 print("[INFO]Loaded {} negative examples".format(len(negative_paths)))
@@ -24,7 +26,7 @@ def examples_to_dataset(examples, block_size = 2):
         img = imread(path, as_grey = True)
         img = block_reduce(img, block_size=(block_size, block_size), func = np.mean)
         X.append(img)
-        y.append(img)
+        y.append(label)
     
     return np.asarray(X), np.asarray(y)
 
@@ -32,3 +34,22 @@ X, y = examples_to_dataset(examples)
 
 end = time.time()
 print("[INFO]Time taken - {}s".format(end - start))
+
+# Turn X and Y into numpy arrays and coerce them into right shape and range
+X = X.astype(np.float32) / 255.
+y = y.astype(np.int32)
+
+print("[INFO]X : Datatype - {} Minimum Value - {} Maximum Value - {} Shape - {}".format(X.dtype, X.min(), X.max(), X.shape))
+print("[INFO]Y : Datatype - {} Minimum Value - {} Maximum Value - {} Shape - {}".format(y.dtype, y.min(), y.max(), y.shape))
+
+# Show negative examples
+show_array(a = 255 * make_mosaic(X[:len(negative_paths)], 8), format='png', filename='Neg.png')
+# Show positive examples
+show_array(a = 255 * make_mosaic(X[-len(positive_paths):], 8), format='png', filename='Pos.png')
+
+# Save data to disk
+X = np.expand_dims(X, axis = -1)
+np.save('X.npy', X)
+np.save('y.npy', y)
+print("[INFO}Shape of X after expanding dimensions - {}", X.shape)
+
--- SMILE-CNN/utils/show_array.py
+++ SMILE-CNN/utils/show_array.py
@@ -1,17 +1,10 @@
-from cStringIO import cStringIO
 import numpy as np 
-import PIL.Image 
-import IPython.display
-import shutil 
+import cv2
 
 def show_array(a, format='png', filename = None):
     a = np.squeeze(a)
     a = np.uint8(np.clip(a, 0, 255))
-    image_data = StringIO()
-    PIL.Image.fromarray(a).save(image_data, format)
-    if filename is None:
-        IPython.display.display(IPython.display.Image(data = image_data.get_value()))
-    else:
-        with open(filename, 'w') as f:
-            image_data.seek(0)
-            shutil.copyfileobj(image_data, f)
+   
+    cv2.imwrite(filename, a)
+    cv2.imshow(str(filename), a)
+    cv2.waitKey(0)
