f3a683bfa6a10cec66f846a4092ab825144f6cfe
sayands <sayandsarkar.1997@gmail.com>
2019-02-07 19:56:23 +0530

    Added the script for mixed training architecture

---
+++ House-Price-Prediction/Mixed-Input-Model/utils/models.py
@@ -0,0 +1,60 @@
+# Importing the neccessary packages and libraries
+from keras.models import Sequential
+from keras.layers.normalization import BatchNormalization
+from keras.layers.convolutional import Conv2D
+from keras.layers.convolutional import MaxPolling2D
+from keras.layers.core import Activation
+from keras.layers.core import Dropout
+from keras.layers.core import Dense 
+from keras.layers import Flatten, Input
+from keras.models import Model 
+
+def create_mlp(dim, regress = False):
+    # define our MLP network
+    model = Sequential()
+    model.add(Dense(8, input_dim = dim, activation = "relu"))
+    model.add(Dense(4, activation = "relu"))
+
+    if regress:
+        model.add(Dense(1, activation = "linear"))
+    
+    # return our model 
+    return model 
+
+def create_cnn(width, height, depth, filters=(16, 32, 64), regress = False):
+    
+    inputShape = (height, width, depth)
+    chanDim = -1 
+
+    # define the model input
+    inputs = Input(shape = inputShape)
+
+    # loop over the no.of filters
+    for (i, f) in enumerate(filters):
+        # if this is the first CONV Layer then set the input
+        # appropriately
+        if i == 0:
+            x = inputs 
+
+        # CONV => RELU => BN => POOL
+        x = Conv2D(f, (3, 3), padding = "same")(x)
+        x = Activation("relu")(x)
+        x = BatchNormalization(axis = chanDim)(x)
+        x = MaxPolling2D(pool_size = (2, 2))(x)
+
+    # Flatten the volume, FC=>RELU=>BN=>DROPOUT
+    x = Flatten()(x)
+    x = Dense(16)(x)
+    x = Activation("relu")(x)
+    x = BatchNormalization(axis = chanDim)(x)
+    x = Dropout(0.5)(x)
+
+    x = Dense(4)(x)
+    x = Activation("relu")(x)
+
+    if regress:
+        x = Dense(1, activation = "linear")(x)
+    
+    model = Model(inputs, x)
+
+    return model 
